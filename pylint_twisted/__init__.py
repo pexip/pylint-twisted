c19a69b46a25649e491a06da0d0a607fff09a8e5
Huw Jones <huw@pexip.com>
2021-02-19 16:55:42 +0000

    Improve reactor inference

--- pylint_twisted/__init__.py
+++ pylint_twisted/__init__.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-"""Brains for helping silence pylint errors/warnings from defer.inlineCallbacks."""
+"""Brains for helping silence pylint errors/warnings from twisted"""
 import astroid
 
 
@@ -31,29 +30,46 @@ astroid.MANAGER.register_transform(
     _is_inlineCallbacks)
 
 
-def _is_reactor_module(node):
-    """Check whether a FunctionDef node is decorated with defer.inlineCallbacks."""
-    return node.name == 'twisted.internet.reactor'
+# == Infer twisted.internet.reactor type ==
+# Because of the abhorrent twisted sys.modules hackery, pylint can't find the reactor, so
+# we're going to help.
+# To find the reactor, we import the default reactor for the platform and mock install it
+# to get our hands on the actual reactor that would be used.
+# Next we use inspect to get the reactor module/class name.
+# Finally we load this as an AST node and transform the AST to refer to this node whenever
+# the reactor module is referenced.
+
+
+def _get_reactor_impl():
+    import inspect
+    from unittest import mock
+    from twisted.internet.default import install
+
+    with mock.patch("twisted.internet.main.installReactor") as mock_install:
+        install()
+    reactor = mock_install.call_args[0][0]
+    reactor_module_name = inspect.getmodule(reactor).__name__
+    reactor_module = astroid.MANAGER.ast_from_module_name(reactor_module_name)
+    return reactor_module[type(reactor).__name__].instantiate_class()
+
+
+_REACTOR_IMPL = _get_reactor_impl()
+
+
+def _is_reactor_module(module):
+    """Check whether a Module node is the twisted reactor"""
+    return module.name == "twisted.internet.reactor"
+
 
 @astroid.inference_tip
-def _infer_inlineCallbacks(node, context=None):  # pylint: disable=unused-argument
-    """Infer the type of inlineCallbackss."""
+def _infer_reactor_module(_module, _context=None):
+    return iter([_REACTOR_IMPL])
 
-    # Does the name of the function matter?  Should it be global?
-    module = astroid.parse("""
-    from twisted.internet import default
-    default.install()
-    """)
-    inlineCallbacks_function = next(
-        module.igetattr(node.name, context=context))
-    print(inlineCallbacks_function)
-    return iter([inlineCallbacks_function])
 
 astroid.MANAGER.register_transform(
-    astroid.Module,
-    _infer_inlineCallbacks,
-    _is_reactor_module)
+    astroid.Module, _infer_reactor_module, _is_reactor_module
+)
 
-def register(linter):  # pylint: disable=unused-argument
-    """Register the plugin with the linter."""
-    pass
+
+def register(linter):
+    """Required to register the plugin with pylint"""
