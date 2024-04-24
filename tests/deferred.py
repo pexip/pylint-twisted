ae0c5db97eee58e232c3e08d00174e836dd1fbd7
Huw Jones <huw@pexip.com>
2024-04-24 10:14:41 +0100

    tests/functional/deferred: add test to ensure args are still validated

--- tests/deferred.py
+++ tests/deferred.py
@@ -3,11 +3,12 @@ from twisted.internet import reactor, defer
 
 
 def check_was_called():
-    dfrd = deferred()
+    dfrd = do_thing(1, "hello")
     if dfrd.called:
         print(dfrd.was_called)  # [no-member]
 
 
 @defer.inlineCallbacks
-def deferred():
+def do_thing(a: int, b: str):
     yield reactor.callLater(1, lambda: None)
+    print(f"{b}: {a}")
