13575fd0a183f4a24e76a117be1d311a1105d894
Huw Jones <huw@pexip.com>
2024-04-24 11:19:11 +0100

    inference/deferred: prevent decorators from being dropped

--- tests/deferred.py
+++ tests/deferred.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,invalid-name,too-few-public-methods
 from twisted.internet import reactor, defer
 
 
@@ -7,8 +7,24 @@ def check_was_called():
     if dfrd.called:
         print(dfrd.was_called)  # [no-member]
 
+    yield Foo.why_would_you_do_this(True)
+    yield Foo.just_no(False)
+
 
 @defer.inlineCallbacks
 def do_thing(a: int, b: str):
     yield reactor.callLater(1, lambda: None)
     print(f"{b}: {a}")
+
+
+class Foo:
+
+    @classmethod
+    @defer.inlineCallbacks
+    def why_would_you_do_this(cls, grrrr) -> None:
+        yield defer.succeed(grrrr)
+
+    @staticmethod
+    @defer.inlineCallbacks
+    def just_no(why) -> None:
+        yield defer.succeed(why)
